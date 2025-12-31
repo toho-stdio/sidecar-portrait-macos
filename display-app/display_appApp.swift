c5146d3af4b119d1d8e2619495f5d4417c9540ef
toanhx <toanhodev@gmail.com>
2025-12-31 12:22:07 +0700

    Initial Commit

---
+++ display-app/display_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  display_appApp.swift
+//  display-app
+//
+//  Created by toho on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct display_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ display-appTests/display_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  display_appTests.swift
+//  display-appTests
+//
+//  Created by toho on 31/12/25.
+//
+
+import Testing
+@testable import display_app
+
+struct display_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
